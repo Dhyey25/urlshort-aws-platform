3d1acd145b51d42d3d361f066378ac0f4226e130
Dhyey Solanki <dhyeysolanki@gmail.com>
2026-05-11 19:42:56 -0400

    Addind CD to workflow (Trivy Fix)

--- infra/modules/github-oidc/main.tf
+++ infra/modules/github-oidc/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
 variable "github_org" {
   type = string
 }
